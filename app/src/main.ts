073dae6890751c3500889e88bcf06f112db6e515
Nam Hoang <hoangxuannam160493@gmail.com>
2024-08-28 15:32:28 +0700

    chore: integrate serverless
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { validationExceptionFactory } from './common/factories/validation-exception.factory';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -28,6 +32,27 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+
+  if (process.env.NODE_ENV === 'production') {
+    const expressApp = app.getHttpAdapter().getInstance();
+    return serverlessExpress({ app: expressApp });
+  } else {
+    return await app.listen(3000);
+  }
 }
-bootstrap();
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+// Lambda handler function to bootstrap NestJS application
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+// TODO: Add another functions for Google Cloud Functions and Azure Functions
